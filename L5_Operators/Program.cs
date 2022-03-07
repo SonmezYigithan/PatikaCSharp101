3441746bc9a1c7c0a41f608989b789c9e7f264a0
SonmezYigithan <sonmez.yigithan@gmail.com>
2022-03-07 23:32:58 +0300

    L5_Opearators

---
+++ L5_Operators/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace L5_Operators{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 3;
+            y = y+2;
+            y += 2;
+
+            Console.WriteLine(y);
+            y /= 1;
+            Console.WriteLine(y);
+            x *= 2;
+            Console.WriteLine(x);
+
+            // mantıksal Operatörler
+
+            bool isSuccess = true;
+            bool isComplete = false;
+
+            if( isSuccess && isComplete ){
+                Console.WriteLine("Success");
+            }
+            else if( isSuccess || isComplete ){
+                Console.WriteLine("Success or Complete");
+            }
+            else if ( !isSuccess && !isComplete ){
+                Console.WriteLine("Fail");
+            }
+            else{
+                Console.WriteLine("Unknown");
+            }
+
+
+            // ilişkisel operatörler
+
+            int a = 5;
+            int b = 3;
+            bool sonuc = a < b;
+
+            Console.WriteLine(sonuc);
+            sonuc = a > b;
+            Console.WriteLine(sonuc);
+            sonuc = a <= b;
+            Console.WriteLine(sonuc);
+            sonuc = a >= b;
+            Console.WriteLine(sonuc);
+            sonuc = a == b;
+            Console.WriteLine(sonuc);
+            sonuc = a != b;
+            Console.WriteLine(sonuc);
+
+            Console.WriteLine("**** Aritmatik Operatorler ****");
+            int c = 5;
+            int d = 3;
+            int sonuc1 = c / d;
+            Console.WriteLine(sonuc1);
+            sonuc1 = c % d;
+            Console.WriteLine(sonuc1);
+            sonuc1 = c + d;
+            Console.WriteLine(sonuc1);
+            
+            // % mod alma
+
+            int sonuc2 = 20%3;
+            Console.WriteLine(sonuc2);
+            
+        }
+    }
+}
